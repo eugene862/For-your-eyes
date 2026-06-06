545d29ce995eafa84b9580034a3d27ca7f8553bc
eugene862 <151839594+eugene862@users.noreply.github.com>
2026-06-06 15:42:35 +0300

    Update eyes.js

--- eyes.js
+++ eyes.js
@@ -1,7 +1,7 @@
 // Register Service Worker for PWA compliance
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
+    navigator.serviceWorker.register('sw.js')
       .then(reg => console.log('Service Worker registered successfully.'))
       .catch(err => console.log('Service Worker registration failed:', err));
   });
